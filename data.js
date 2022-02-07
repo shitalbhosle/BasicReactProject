398ef5934e6cd3c9d51246659f3cab45c81693ec
shitalbhosle <94833435+shitalbhosle@users.noreply.github.com>
2022-02-07 12:26:25 +0530

    birthday-remainder

---
+++ data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        id: 1,
+        name: 'Priyanka Dolse',
+        age: 22,
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    },
+    {
+        id: 2,
+        name: 'Abhishek Bhosle',
+        age: 19,
+        image:
+            'https://www.campbellcoproperty.com/wp-content/uploads/2019/06/pexels-photo-220453.jpeg',
+    },
+    {
+        id: 3,
+        name: 'Shital Bhosle',
+        age: 22,
+        image:
+            'http://mariettadegrant.com/wp-content/uploads/2021/02/take-care-of-your-edges.jpeg',
+    },
+    {
+        id: 4,
+        name: 'Shweta Ghadge',
+        age: 22,
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+    {
+        id: 5,
+        name: 'Akanksha Kumbar',
+        age: 22,
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+];
