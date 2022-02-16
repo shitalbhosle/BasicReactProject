edcda920e9fb1e518d9361e46667ec0009ff376a
shitalbhosle <94833435+shitalbhosle@users.noreply.github.com>
2022-02-16 16:08:38 +0530

    sidebarProject

---
+++ Sidebar/Home.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react'
+import { FaBars } from 'react-icons/fa'
+import { AppContext, useGlobalContext } from './Context'
+
+const Home = () => {
+  const  {openSidebar, openModal} = useGlobalContext()
+  
+  return (
+      <main>
+          <button className='sidebar-toggle' onClick={openSidebar}>
+              <FaBars/>
+              </button>
+              <button className='btn' onClick={openModal}>Show Modal</button>
+          </main>
+  )
+}
+
+export default Home
